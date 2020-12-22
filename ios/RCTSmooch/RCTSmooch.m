acb9a40f9afeef5b0f7ad27b372a691a53cbfb51
Jonathan Heinz <jheinzdev@gmail.com>
2020-12-22 09:39:24 -0800

    event names match

--- ios/RCTSmooch/RCTSmooch.m
+++ ios/RCTSmooch/RCTSmooch.m
@@ -171,7 +171,7 @@ RCT_EXPORT_MODULE();
 
 - (NSArray<NSString *> *)supportedEvents
 {
-  return @[@"unreadCountUpdate", @"message", @"participant:added", @"participant:removed"];
+  return @[@"unreadCount", @"message", @"participant:added", @"participant:removed"];
 }
 
 - (BOOL)isInteger:(NSString *)toCheck {
