8b2499c69fd5118625db767b210dd3826969362a
Jonathan Heinz <jheinzdev@gmail.com>
2021-01-07 14:02:30 -0800

    android message event going through at correct time

--- android/src/main/java/com/smooch/rnsmooch/ReactNativeSmooch.java
+++ android/src/main/java/com/smooch/rnsmooch/ReactNativeSmooch.java
@@ -467,6 +467,17 @@ public class ReactNativeSmooch extends ReactContextBaseJavaModule {
                     Log.d("Smooch", String.valueOf(globalMetadata));
                     message.setMetadata(getProperties(globalMetadata));
                 }
+
+                User user = User.getCurrentUser();
+
+                WritableMap result = new WritableNativeMap();
+                result.putString("id", message.getId());
+                result.putString("date", message.getDate().toString());
+                result.putString("text", message.getText());
+                result.putString("author", user.getUserId());
+                result.putString("conversationId", activeConversationId);
+                result.putMap("metadata", convertMapToReactNativeMap(message.getMetadata()));
+                sendEvent(mreactContext, "message", result);
                 return message;
             }
 
@@ -542,16 +553,6 @@ public class ReactNativeSmooch extends ReactContextBaseJavaModule {
             public void onMessageSent(@NonNull Message message, @NonNull MessageUploadStatus messageUploadStatus) {
                 Log.d("__SMOOCH__ sending", message.getId());
                 Log.d("__SMOOCH__ message date", message.getDate().toString());
-                User user = User.getCurrentUser();
-
-                WritableMap result = new WritableNativeMap();
-                result.putString("id", message.getId());
-                result.putString("date", message.getDate().toString());
-                result.putString("text", message.getText());
-                result.putString("author", user.getUserId());
-                result.putString("conversationId", activeConversationId);
-                result.putMap("metadata", convertMapToReactNativeMap(message.getMetadata()));
-                sendEvent(mreactContext, "message", result);
             }
 
             @Override
